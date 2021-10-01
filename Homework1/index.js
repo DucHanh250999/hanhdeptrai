c99ce3ce01e4dc9a1518010cce3069261687e180
DucHanh250999 <duchanh0525@gmail.com>
2021-10-01 22:01:44 +0700

    up files

---
+++ Homework1/index.js
@@ -0,0 +1,53 @@
+// ý a
+
+// let message = ('Coding is great');
+// console.log(message);
+
+// ý b
+
+// let studentCount = 0;
+// console.log(studentCount);
+
+// Bài 4 
+
+// ý a
+// message = ('Coding might not be easy, but still great');
+// console.log(message);
+// ý b
+// studentCount = 24;
+// console.log(studentCount);
+// ý c
+// studentCount = studentCount + 5;
+// console.log(studentCount);
+// let x = 5;
+// x = x + 5;
+// console.log(x);
+// let x = Number(prompt('nhập vào một số: '));
+// x = x + 5;
+// console.log(x);
+ 
+// bài 5
+// alert('You lool beautiul today');
+
+// bài 6
+// let x = prompt('Hi there, your name please ?');
+// alert(`Hi ${x}`);
+
+// Bài 7
+// let x = prompt('Enter your first name :');
+// let y = prompt('Enter your last name:');
+// alert(`Hi ${y} ${x}`);
+
+// Bài 8
+// let x = prompt('Enter side length of the aquare:');
+// alert(`The square area is: ${x*x}`);
+
+// Bài 9
+let r = prompt('Nhập vào bán kính:');
+let s = r * r * 3.14 ;
+alert(`Diện tích hình tròn là: ${s}`);
+
+// Bài 10
+// let t = prompt('Nhập vào độ C :');
+// alert(`${t} (C) = ${t*(9/5)+32} (F)`);
+
